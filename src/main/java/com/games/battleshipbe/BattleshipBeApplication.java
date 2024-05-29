97171cdc0ff18581e8cf54ffd4215be94ea4ab88
vssavin <savin1801@gmail.com>
2024-05-29 10:53:59 +0300

    project init

---
+++ src/main/java/com/games/battleshipbe/BattleshipBeApplication.java
@@ -0,0 +1,13 @@
+package com.games.battleshipbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleshipBeApplication.class, args);
+    }
+
+}

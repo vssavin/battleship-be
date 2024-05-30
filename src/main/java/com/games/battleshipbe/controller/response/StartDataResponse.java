2989e62b5b2c9ff9e105a1f1b4b4119e4e7d844a
vssavin <savin1801@gmail.com>
2024-05-30 15:58:14 +0300

    add data response classes

---
+++ src/main/java/com/games/battleshipbe/controller/response/StartDataResponse.java
@@ -0,0 +1,12 @@
+package com.games.battleshipbe.controller.response;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class StartDataResponse {
+    private Long gameId;
+}

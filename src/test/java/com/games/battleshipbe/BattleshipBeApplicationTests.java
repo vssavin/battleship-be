6e96d2dd2fce5405fce31867f946cac47fe7112c
vssavin <savin1801@gmail.com>
2024-05-30 16:45:29 +0300

    add init scripts for test class

--- src/test/java/com/games/battleshipbe/BattleshipBeApplicationTests.java
+++ src/test/java/com/games/battleshipbe/BattleshipBeApplicationTests.java
@@ -2,12 +2,15 @@ package com.games.battleshipbe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
+@Sql(scripts = {"classpath:test_schema.sql", "classpath:test_data.sql"})
 class BattleshipBeApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
